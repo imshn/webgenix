5c544b46c85c2221f4257a169cf8e59726aca822
imshn <shaan@vyomr.in>
2024-05-12 22:38:44 +0530

    Creared SRC Folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const nodemailer = require('nodemailer');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.urlencoded({ extended: true }))
 // Define routes
 
